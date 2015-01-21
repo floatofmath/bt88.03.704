23f6ad3fbb065a3afae7f12db3a553990794b257
Florian Klinglmueller <float@lefant.net>
2015-01-21 14:22:08 +0100

    added plotting options, and linking to UCSC browser

--- R/edger.R
+++ R/edger.R
@@ -3,7 +3,7 @@
 ##' @title edgeR wrapper
 ##' @param data a DGEList object
 ##' @param MM a model matrix
-##' @param CM a contrast matrix
+##' @param CM a contrast matrix if NULL no contrasts will be fit
 ##' @param ... additional parameters passed to glmLRT
 ##' @return a DGELRT object
 ##' @author Florian Klinglmueller
@@ -45,3 +45,54 @@ myTopTags <- function(lrts,N=NULL,annodata=NULL,key='nearest_ref_id'){
     }
     table[1:N]
 }
+
+##' Make a link to the projects UCSC Browser page
+##'
+##' @title UCSC Browser link
+##' @param locus  transcript location in the form "loc:start-end"
+##' @param project  url string giving the project page at ucsc
+##' @return a character string with the url to the tag in the ucsc
+##' @author float
+##' @export
+makeUCSClink <- function(locus,project="&hgsid=201952662_blwe2mLIAlJ066Rb2rEAvZDqZiGz"){
+    locus <- sub(":","%3A",locus)
+    paste0("http://genome-euro.ucsc.edu/cgi-bin/hgTracks?db=mm10&position=",locus,project)
+}
+
+##' Make a heatmap from myTopTags
+##'
+##' @title TopTag heatmap
+##' @param tt data.table results from myTopTags
+##' @param groups sample groups to be outlined in the plot
+##' @param dataCols columns with data to be shown
+##' @param anno columns with tag identifiers
+##' @return 
+##' @author float
+plotTopTags <- function(tt,groups,dataCols,anno="gene_short_name"){
+    library(RColorBrewer)
+    library(gplots)
+    data <- as.data.frame(tt[,dataCols,with=F])
+    rownames(data) <- tt[[anno]]
+    heatslide(data,tt[,logFC],groups,bluered(32),brewer.pal(length(levels(groups)),name='Accent'))
+}
+
+##' Apply non-specific filtering to DGEList
+##'
+##' @title Filter DGE
+##' 
+##' @param dge DGEList with transcript reads
+##' @param mCt minimum count 
+##' @param atleast number of samples for which count has to exceed \code{mCT} 
+##' @param topM number transcripts with largest MAD to be selected for further processing 
+##' @return DGEList with filtered transcripts
+##' @author Florian Klinglmueller
+filterDGE <- function(dge,mCt=1.5,atleast=2,topM=8000){
+    thresh <- rowSums(cpm(deg)>mCt) >= atleast
+    dge <- dge[thresh,]
+    mad <- rank(-apply(cpm(dge),1,mad)) <= topM
+    dge <- dge[mad,]
+    dge$samples$lib.size <- colSums(dge$counts)
+    dge <- calcNormFactors(filtered)
+    dge
+}
+    
