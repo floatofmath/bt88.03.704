069c4d06f4813e8f810cb0fae643003a8139bd0a
float <float@lefant.net>
2015-02-27 10:07:01 +0100

    modified template so that figuer captions are included by default

--- R/markdown.R
+++ R/markdown.R
@@ -8,10 +8,10 @@ html_statement <- function(toc = FALSE){
     css <- system.file("statement/statement.css",package="bt88.03.704")
     template <- system.file("statement/statement.html",package="bt88.03.704")
     
-    toc <- TRUE
     rmarkdown::html_document(toc=toc,
                              fig_width=6.5,
                              fig_height=4,
+                             fig_caption=TRUE,
                              theme=NULL,
                              css = css,
                              template = template)
